c3147173f44c6fe20940653e99355c28dee2d3d6
Alberto González Balderas <albertoglezb@hotmail.com>
2021-12-09 11:44:48 -0600

    The code was aligned to the code in the course.

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const fs = require("fs");
 const results = [];
 
 fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
   .on("data", (data) => {
     results.push(data);
   })
   .on("error", (err) => {
-    console.error(err);
+    console.log(err);
   })
   .on("end", () => {
     console.log(results);
-    console.log("done");
   });
-
-// parse();
